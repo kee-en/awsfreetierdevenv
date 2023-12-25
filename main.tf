ad29614a00933a2f71f367ee92687d0d210c1ba9
Kien Dolorfo <kiendolorfo@gmail.com>
2023-12-25 21:48:42 +0800

    Add Files

---
+++ main.tf
@@ -0,0 +1,52 @@
+provider "aws" {
+  region = "ap-southeast-1"
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"]
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+}
+
+resource "aws_key_pair" "my_key_pair" {
+  key_name   = "Mac-KP"
+  public_key = file("~/.ssh/id_rsa.pub")
+}
+
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow inbound SSH traffic"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "development_instance" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "DevelopmentInstance"
+  }
+
+  key_name = aws_key_pair.my_key_pair.key_name
+  security_groups = [ aws_security_group.allow_ssh.id ]
+}
+
+output "public_ip" {
+  value = aws_instance.development_instance.public_ip
+}
