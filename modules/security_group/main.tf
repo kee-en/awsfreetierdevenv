5db0199ac9a7ab3286dac9a8fb9d72822e9df7d1
Kien Dolorfo <kiendolorfo@gmail.com>
2024-01-22 19:51:26 +0800

    Add Security Group module

---
+++ modules/security_group/main.tf
@@ -0,0 +1,27 @@
+resource "aws_security_group" "allow_development_ports" {
+  name        = "${var.resource_name}-sg"
+  description = "Allow inbound development traffic"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = var.tags
+}
