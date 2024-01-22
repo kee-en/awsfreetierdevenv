757bcb5f1b5f924050bade647d0ea8e8c4ae6b03
Kien Dolorfo <kiendolorfo@gmail.com>
2024-01-22 19:51:16 +0800

    Add Security Group module

--- modules/key_pair/main.tf
+++ modules/key_pair/main.tf
@@ -1,5 +1,5 @@
 resource "aws_key_pair" "my_key_pair" {
-  key_name = "${var.resource_name}-key-pair"
+  key_name   = "${var.resource_name}-key-pair"
   public_key = file("~/.ssh/id_ed25519.pub")
 
   tags = var.tags
--- modules/key_pair/output.tf
+++ modules/key_pair/output.tf
@@ -1,3 +1,3 @@
-output "key_pair_name" {
+output "name" {
   value = aws_key_pair.my_key_pair.key_name
 }
